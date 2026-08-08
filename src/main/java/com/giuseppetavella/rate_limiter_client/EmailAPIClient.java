3f1ea5610e0fdc53194cf7142697e1ae3793bed2
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-08-08 02:47:51 +0200

    email client with completable future

--- src/main/java/com/giuseppetavella/rate_limiter_client/EmailAPIClient.java
+++ src/main/java/com/giuseppetavella/rate_limiter_client/EmailAPIClient.java
@@ -4,27 +4,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 
 public class EmailAPIClient {
-    private final String sender;
     private final String url;
+    private final ExecutorService executor;
     
     public EmailAPIClient() {
-        this.sender = "sender@example.com";
         this.url = "http://localhost:9000/email-api";
+        this.executor = Executors.newVirtualThreadPerTaskExecutor();
     }
     
-    public void sendEmail(String recipient, String subject, String body)  {
+    public CompletableFuture<ResponseEntity<String>> sendEmail(String recipient, String subject, String body)  {
         var restTemplate = new RestTemplate();
-        
-        ResponseEntity<String> response = restTemplate.postForEntity(url, new Object(), String.class);
-
-        System.out.println(response);
-        
         // Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
         
+        return CompletableFuture.supplyAsync(() -> {
+            ResponseEntity<String> response = restTemplate.postForEntity(url, new Object(), String.class);
+            return response;
+        }, executor);
     }
     
 }
--- src/test/java/com/giuseppetavella/rate_limiter_client/EmailAPIClientTest.java
+++ src/test/java/com/giuseppetavella/rate_limiter_client/EmailAPIClientTest.java
@@ -2,17 +2,31 @@ package com.giuseppetavella.rate_limiter_client;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailAPIClientTest {
     @Test
     void test1() {
         var client = new EmailAPIClient();
-        client.sendEmail("s","s","qq");
-        client.sendEmail("s","s","qq");
-        client.sendEmail("s","s","qq");
-        client.sendEmail("s","s","qq");
-        client.sendEmail("s","s","qq");
+
+        List<CompletableFuture<?>> cfs = new ArrayList<>();
+        cfs.add(client.sendEmail("s","s","qq"));
+        cfs.add(client.sendEmail("s","s","qq"));
+        cfs.add(client.sendEmail("s","s","qq"));
+        
+        CompletableFuture.allOf(
+          cfs.toArray(new CompletableFuture[0])
+        ).join();
+
+        for(var cf : cfs) {
+            System.out.println(cf.join());            
+        }
+        
         // client.sendEmail("s","s","qq");
         
     }
